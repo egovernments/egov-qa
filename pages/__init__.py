60e89c825a985a26e7a432e62270246b2ed69fa5
Abhilashegov <35691559+Abhilashegov@users.noreply.github.com>
2018-03-27 09:48:07 +0530

    Changes updated: mail to email, use of assert and removing print statement on reusable method (#5)
    * Test cases for citizenProfile and Complain Submitted
    
    * Changes updated: mail to email, use if assert and removing print statement on reusable method
    
    * Adding MyComplaints.py file and updating test_registration.py

--- pages/__init__.py
+++ pages/__init__.py
@@ -3,5 +3,8 @@ from .Registration import OTPPage
 from .CreateComplaint import (
     AddComplaintPage, ComplaintTypePage
 )
+from .ComplaintSubmitted import (
+ComplaintSubmittedPage
+)
 
 from .UserRegistration import RegistrationPage
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,5 +1,8 @@
 from pages import OTPPage, AddComplaintPage, RegistrationPage
 from framework.selenium_plus import *
+from pages.CitizenProfile import CitizenProfilePage
+from pages.ComplaintSubmitted import ComplaintSubmittedPage
+from pages.MyComplaints import MyComplaintsPage
 
 
 def test_otp_submission():
@@ -9,11 +12,35 @@ def test_otp_submission():
     otp.set("1234").submit()
     assert get_url() == "http://egov-micro-dev.egovernments.org/app/v3/citizen"
 
+
 def test_create_complaint():
     comp = AddComplaintPage
     comp.set_complaint_type("garbage", "Overflowing Garbage Bins")
 
     assert get_url() == "http://egov-micro-dev.egovernments.org/app/v3/citizen/add-complaint"
+
+
+def test_citizen_profile():
+    CitizenProfilePage.navigate().set("ABH", "ab.se@gmail.com").set_city("Amritsar").click_continue()
+
+def test_complain_submitted():
+    ComplaintSubmittedPage.navigate()
+    print(ComplaintSubmittedPage.get_complaint_number())
+    ComplaintSubmittedPage.click_continue()
+    assert get_url() == "http://egov-micro-dev.egovernments.org/app/v3/citizen"
+
+def test_my_complaints():
+    complaints = MyComplaintsPage
+    complaints.navigate()
+    cards = complaints.get_all_complaints()
+    card = cards[2]
+
+    print (card.get_complaint_header())
+    print( card.get_complaint_status())
+    print(card.get_complaint_no())
+    print(card.get_complaint_date())
+    card.track_complaint()
+    assert get_url() == "http://egov-micro-dev.egovernments.org/app/v3/citizen/complaint-details?status=rejected"
 
 def test_user_registration():
     userRegistration = RegistrationPage
